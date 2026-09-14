8f1152b654e99adcb0e058aa48bc09356fcf2413
semohabiby7-jpg <semohabiby7@gmail.com>
2026-09-14 12:29:06 +0000

    إصلاح: gemini-3.6-flash (الـ model الصحيح المتاح)

--- worker-v2.js
+++ worker-v2.js
@@ -42,7 +42,7 @@ export default {
       ];
 
       const res = await fetch(
-        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=' + apiKey,
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent?key=' + apiKey,
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
